2cc27525bed2e0b5a7eb9016e0d1ee05657d5185
bujowskis <s.bujowski@gmail.com>
2022-01-26 11:48:10 +0100

    add conceptual GUI

--- stocks-simulator/src/main/java/com/example/stockssimulator/uicomponents/initialization/InitializationController.java
+++ stocks-simulator/src/main/java/com/example/stockssimulator/uicomponents/initialization/InitializationController.java
@@ -2,17 +2,11 @@ package com.example.stockssimulator.uicomponents.initialization;
 
 import com.example.stockssimulator.MainScreen;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.net.URL;
 
 /**
  * Controller for Initialization Screen
--- stocks-simulator/src/main/java/com/example/stockssimulator/uicomponents/simulation/SimulationViewController.java
+++ stocks-simulator/src/main/java/com/example/stockssimulator/uicomponents/simulation/SimulationViewController.java
@@ -1,11 +1,73 @@
 package com.example.stockssimulator.uicomponents.simulation;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
 /**
  * Combines all main UI components into a single whole
  */
 public class SimulationViewController {
+    public GridPane gridPaneMain;
+    public VBox vBoxStocks;
+    public ListView listViewStocks;
+    public VBox vBoxInvestors;
+    public ListView listViewInvestors;
+    public VBox vBoxStockOverview;
+    public VBox vBoxInvestorOverview;
+    public VBox vBoxOwnedStocks;
+    public ListView listViewOwnedStocks;
+    public VBox vBoxStockChart;
+    public LineChart lineChartStock;
+    public VBox vBoxGlobalMenu;
+    public Label labelCurrentTime;
+    public Label labelTimeRate;
+    public Button buttonTimeRate1;
+    public Button buttonTimeRate5;
+    public Button buttonTimeRate10;
+    public Button buttonTimeRate30;
+    public Button buttonTimeRate60;
+    public Button buttonTimeRate120;
+    public Button buttonTimeRate300;
+
     // todo
     public SimulationViewController() {}
+
+    /*
+    // GLOBAL MENU
+    // todo - these could be done as one function, with single argument (?)
+    @FXML
+    public void setTimeRate1() {
+        // todo
+    }
+    @FXML
+    protected void setTimeRate5() {
+        // todo
+    }
+    @FXML
+    protected void setTimeRate10() {
+        // todo
+    }
+    @FXML
+    protected void setTimeRate30(){
+        // todo
+    }
+    @FXML
+    protected void setTimeRate60() {
+        // todo
+    }
+    @FXML
+    protected void setTimeRate120() {
+        // todo
+    }
+    @FXML
+    protected void setTimeRate300() {
+        // todo
+    }
+     */
 }
